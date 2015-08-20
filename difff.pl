7f2c60c0bb235ae2b3a3e5bb14cfe39a29446d00
meso_cacase <y-naito@dbcls.rois.ac.jp>
2015-08-20 22:05:24 +0900

    説明へリンク

--- difff.pl
+++ difff.pl
@@ -305,7 +305,9 @@ $message =~ s{^(ERROR.*)$}{<p><font color=red>$1</font></p>}s ;
 <p>新着情報：</p>
 
 <ul>
-	<li>2015-06-17　ﾃﾞｭﾌﾌの結果を公開する機能を追加 (ver.6.1)
+	<li>2015-06-17　ﾃﾞｭﾌﾌの結果を公開する機能を追加 (ver.6.1) -
+		<a target='_blank' href='http://g86.dbcls.jp/~meso/meme/archives/2957'>
+			説明</a>
 	<li>2014-03-14　トップページURLを <a href='http://difff.jp/'>http://difff.jp/</a> に変更
 	<li>2014-03-12　ITmediaニュース -
 		<a target='_blank' href='http://www.itmedia.co.jp/news/articles/1403/12/news121.html'>
@@ -331,7 +333,7 @@ $message =~ s{^(ERROR.*)$}{<p><font color=red>$1</font></p>}s ;
 
 <hr><!-- ________________________________________ -->
 
-<p><font color=gray>Last modified on Jul 30, 2015 by
+<p><font color=gray>Last modified on Aug 20, 2015 by
 <a target='_blank' href='http://twitter.com/meso_cacase'>@meso_cacase</a>
 </font></p>
 --EOS--
